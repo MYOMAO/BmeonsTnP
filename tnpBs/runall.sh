bf6e8d870753d6627b6da0f4667a97a756b71c04
MYOMAO <zhaozhongshi@Zhaozhongs-MacBook-Air-5.local>
2020-07-05 10:47:26 -0700

    First Commit

---
+++ tnpBs/runall.sh
@@ -0,0 +1,18 @@
+root -b -l -q tnp_converter.cc++'(0,90,0)'
+root -b -l -q tnp_converter.cc++'(0,90,1)'
+root -b -l -q tnp_converter.cc++'(0,90,2)'
+root -b -l -q tnp_converter.cc++'(0,90,3)'
+root -b -l -q tnp_converter.cc++'(0,90,4)'
+
+root -b -l -q tnp_converter.cc++'(0,30,0)'
+root -b -l -q tnp_converter.cc++'(0,30,1)'
+root -b -l -q tnp_converter.cc++'(0,30,2)'
+root -b -l -q tnp_converter.cc++'(0,30,3)'
+root -b -l -q tnp_converter.cc++'(0,30,4)'
+
+
+root -b -l -q tnp_converter.cc++'(30,90,0)'
+root -b -l -q tnp_converter.cc++'(30,90,1)'
+root -b -l -q tnp_converter.cc++'(30,90,2)'
+root -b -l -q tnp_converter.cc++'(30,90,3)'
+root -b -l -q tnp_converter.cc++'(30,90,4)'
